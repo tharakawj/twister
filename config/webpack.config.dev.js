3fb8511490d0f189ed56e80de8b0fc188d9cc335
Tharaka Wijebandara <tharaka.nw@gmail.com>
2017-01-13 20:27:39 +0530

    Initial commit with essential working server and client structure

---
+++ config/webpack.config.dev.js
@@ -0,0 +1,62 @@
+import webpack from 'webpack';
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import InterpolateHtmlPlugin from 'react-dev-utils/InterpolateHtmlPlugin';
+
+// Webpack uses `publicPath` to determine where the app is being served from.
+// In development, we always serve from the root. This makes config easier.
+const publicPath = '/';
+// `publicUrl` is just like `publicPath`, but we will provide it to our app
+// as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
+// Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
+const publicUrl = '';
+
+module.exports = {
+  entry: [
+    path.resolve('./config/polyfills'),
+    path.resolve('./client/index')
+  ],
+  output: {
+    path: path.resolve('./build'),
+    // Add /* filename */ comments to generated require()s in the output.
+    pathinfo: true,
+    publicPath: publicPath,
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        include: path.resolve("./client"),
+        use: [{
+          loader: 'babel-loader',
+          options: { 
+            presets: [
+              'react',
+              ['es2015', { "modules": false }]
+            ],
+            cacheDirectory: true
+          }
+        }],
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      }
+    ]
+  },
+  plugins: [
+    // Makes the public URL available as %PUBLIC_URL% in index.html, e.g.: 
+    // <link rel="shortcut icon" href="%PUBLIC_URL%/favicon.ico"> 
+    new InterpolateHtmlPlugin({
+      PUBLIC_URL: publicUrl
+      // You can pass any key-value pairs, this was just an example. 
+      // WHATEVER: 42 will replace %WHATEVER% with 42 in index.html. 
+    }),
+    // Generates an `index.html` file with the <script> injected. 
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: path.resolve('./public/index.html'),
+    }) 
+  ]
+}
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import Express from 'express';
+import path from 'path';
+
+import serverConfig from '../config/server.config';
+
+// Webpack Requirements
+import webpack from 'webpack';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import config from '../config/webpack.config.dev';
+
+// Initialize the Express App
+const app = new Express();
+
+// Run Webpack middleware in development mode
+if (process.env.NODE_ENV === 'development') {
+  const compiler = webpack(config);
+  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }));
+}
+
+app.use(Express.static(path.resolve(__dirname, '../public')));
+
+// start app
+app.listen(serverConfig.port, (error) => {
+  if (!error) {
+    console.log(`Twister is running on port: ${serverConfig.port}!`);
+  }
+});
